e82908d6363c33503d8dc0a9ef3845897b1c66c0
Jayashakthi Vishnu <vishnu28.js@gmail.com>
2022-02-07 11:13:08 +0530

    optimized random function

--- src/components/home_page/Randomise.js
+++ src/components/home_page/Randomise.js
@@ -1,10 +1,10 @@
-import React, { useContext } from 'react';
-import { gameData } from '../../gameData';
-import { shipData } from './Main';
+import React, { useContext } from "react";
+import { gameData } from "../../gameData";
+import { shipData } from "./Main";
 
 export const Randomise = () => {
-   const {setboardShip,setstaticShips}=useContext(shipData);
-   const staticShips={
+  const { setboardShip, setstaticShips } = useContext(shipData);
+  const staticShips = {
     4: [{ name: "chakra", size: 4, type: "horizontal" }],
     3: [
       { name: "vikranth_1", size: 3, type: "horizontal" },
@@ -20,61 +20,27 @@ export const Randomise = () => {
       { name: "doge_2", size: 1, type: "horizontal" },
       { name: "doge_3", size: 1, type: "horizontal" },
       { name: "doge_4", size: 1, type: "horizontal" },
-    ]
-   }
-   const staticShipsarr=[];
-   for(let key in staticShips){
-       staticShips[key].forEach(element => {
-           staticShipsarr.push(element);
-       });
-   }
-   return <div onClick={(e)=>{
-    setTimeout(() => {
-        randomizer(setboardShip,setstaticShips,staticShipsarr)
-    }, 500);   
-    }}>Randomise</div>;
+    ],
+  };
+  const staticShipsarr = [];
+  let mainObj = {};
+  for (let key in staticShips) {
+    staticShips[key].forEach((element) => {
+      staticShipsarr.push(element);
+    });
+  }
+  return (
+    <div
+      onClick={(e) => {
+        setTimeout(() => {
+          setstaticShips({ 4: [], 3: [], 2: [], 1: [] });
+          setboardShip({});
+          mainObj=gameData.randomizer(staticShipsarr);
+          setboardShip(mainObj);
+        }, 0);
+      }}
+    >
+      Randomise
+    </div>
+  );
 };
-
-function randomizer(setboardShip,setstaticShips,staticShipsarr){
-    setstaticShips({4:[],3:[],2:[],1:[]});
-    gameData.flushData();
-    setboardShip({});
-    staticShipsarr.forEach(d=>{
-        let typeArr=["horizontal","vertical"];
-        let x=+Math.floor(Math.random()*10);
-        let y=x+d.size-1;
-        let r=+Math.floor(Math.random()*10);
-        let type=typeArr[Math.floor(Math.random()*2)];
-        let obj={
-            name:d.name,
-            type:typeArr[Math.floor(Math.random()*2)],
-            size:d.size,
-            index:[x,y,r]
-        }
-        while(gameData.setShipinBoard(x,y,r,type)===false){
-            x=+Math.floor(Math.random()*10);
-            y=x+d.size-1;
-            r=+Math.floor(Math.random()*10);
-            type=typeArr[Math.floor(Math.random()*2)];
-            obj={
-                name:d.name,
-                type:type,
-                index:[x,y,r],
-                size:d.size
-            }
-        }
-        obj={
-            name:d.name,
-            type:type,
-            index:[x,y,r],
-            size:d.size
-        }
-        console.log("Entering here");
-        gameData.setShipObjinBoard(obj,d.name);
-        setboardShip((prev)=>{
-            let newData={...prev};
-            newData[d.name]=obj;
-            return newData;
-        });
-    })
-}
