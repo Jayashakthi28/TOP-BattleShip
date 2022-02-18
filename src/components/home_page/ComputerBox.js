d3e2c35ec8bc42b448a9c749a5062e37775f5e44
Jayashakthi Vishnu <vishnu28.js@gmail.com>
2022-02-18 07:29:23 +0530

    added hit and render logic for computerbox

--- src/components/home_page/ComputerBox.js
+++ src/components/home_page/ComputerBox.js
@@ -1,7 +1,8 @@
 import React, { useContext } from "react";
-import { computer, gameData } from "../../gameData";
-import Grid from "./Grid";
-import { shipData } from "./Main";
+import { computer } from "../../gameData";
+import { BsDot } from "react-icons/bs";
+import { IoMdBoat } from "react-icons/io";
+import { ACTIONS, shipData } from "./Main";
 
 export const ComputerBox = () => {
   const box = [];
@@ -11,8 +12,8 @@ export const ComputerBox = () => {
       box[i].push(j);
     }
   }
-  const computerBoxes=[];
-  const {setcomputerBoard}=useContext(shipData)
+  const computerBoxes = [];
+  const { setcomputerBoard, state, dispatch } = useContext(shipData);
   // const computerShips=computer.getBoardShips();
   // for(let key in computerShips){
   //   let data=computerShips[key];
@@ -25,35 +26,56 @@ export const ComputerBox = () => {
   //     }
   //   }
   // }
-
+  let className=(state.toggler===true)?"box box-right focus animate__animated animate__backInRight":"box box-right animate__animated animate__backInRight";
   return (
-    <div className="box box-right" onClick={(e)=>{
-      let [x,y]=e.target.id.split("");
-      x=+x;
-      y=+y;
-      const change=computer.setHit(x,y,true);
-      if(change){
-        setcomputerBoard((prev)=>{return [...prev]});
-      }
-    }}>
+    <div
+      className={className}
+      onClick={(e) => {
+        if (state.toggler === false || state.toggler===null) return;
+        let [x, y] = e.target.id.split("");
+        x = +x;
+        y = +y;
+        const change = computer.setHit(x, y, true);
+        if (change && computer.getBoard()[x][y].isPlaced) {
+          dispatch({
+            type: ACTIONS.TOGGLER,
+            payload: {
+              data: { toggler: true },
+            },
+          });
+        } else if (change) {
+          setTimeout(()=>{
+            dispatch({
+              type: ACTIONS.TOGGLER,
+              payload: {
+                data: { toggler: false },
+              },
+            });
+          },250);
+        }
+        setcomputerBoard((prev) => {
+          return [...prev];
+        });
+      }}
+    >
       {computer.getBoard().map((k, i) => {
         return computer.getBoard()[i].map((t, j) => {
-          let data=computer.getBoard()[i][j];
-          let classname;
-          if(data.isPlaced && data.isHit){
-            classname="grid hit"
+          let data = computer.getBoard()[i][j];
+          if (data.isPlaced && data.isHit) {
+            return (
+              <div key={`${i}${j}`} id={`${i}${j}`} className="grid">
+                  <IoMdBoat style={{ color: "red" }} size={"50px"} className="animate__animated animate__tada"/>
+              </div>
+            );
+          } else if (!data.isPlaced && data.isHit) {
+            return (
+              <div key={`${i}${j}`} id={`${i}${j}`} className="grid">
+                  <BsDot size={"50px"} className="animate__animated animate__faster animate__headShake"/>
+              </div>
+            );
+          } else {
+            return <div key={`${i}${j}`} id={`${i}${j}`} className="grid" />;
           }
-          else if(!data.isPlaced && data.isHit){
-            classname="grid sunk"
-          }
-          else{
-            classname="grid";
-          }
-          return <div 
-          key={`${i}${j}`} 
-          id={`${i}${j}`}
-          className={classname}
-          />;
         });
       })}
     </div>
