c34ee4b456900455ab2946681563404fd96d1885
Jayashakthi Vishnu <vishnu28.js@gmail.com>
2022-02-18 07:29:36 +0530

    code refactor

--- src/components/home_page/Grid.js
+++ src/components/home_page/Grid.js
@@ -1,25 +1,9 @@
 import React from "react";
 import shipsvg from "../../assets/ship.svg";
-import { gameData } from "../../gameData";
 
 export default function Grid({ isId }) {
   if (isId) {
-    let i,j;
-    [i,j]=isId.split("");
-    i=+i;
-    j=+j;
-    let data=gameData.getBoard()[i][j];
-    let classname;
-    if(data.isPlaced && data.isHit){
-      classname="grid hit"
-    }
-    else if(!data.isPlaced && data.isHit){
-      classname="grid sunk"
-    }
-    else{
-      classname="grid";
-    }
-    return <div className={classname} id={isId}></div>;
+    return <div className="grid" id={isId}></div>;
   }
   return (
     <div className="grid">
